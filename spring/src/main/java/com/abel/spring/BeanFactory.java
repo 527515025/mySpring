602a1272cb07615b75c01984e4849996346cbac3
杨伊博 <527515025@qq.com>
2017-07-06 22:39:42 +0800

    ok

--- spring/src/main/java/com/abel/spring/BeanFactory.java
+++ spring/src/main/java/com/abel/spring/BeanFactory.java
@@ -4,5 +4,5 @@ package com.abel.spring;
  * Created by yangyibo on 17/7/5.
  */
 public interface BeanFactory {
-    public  Object getBean(String name );
+    public  Object getBean(String name);
 }
--- spring/src/test/java/com/abel/service/UserServiceTest.java
+++ spring/src/test/java/com/abel/service/UserServiceTest.java
@@ -21,4 +21,5 @@ public class UserServiceTest {
 		userService.add(new User());
 	}
 
+
 }
