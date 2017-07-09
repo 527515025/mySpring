25a091f74bd33a19aed6926573d88e44f7e6510f
杨伊博 <527515025@qq.com>
2017-07-09 16:01:41 +0800

    OK

--- spring/src/test/java/com/abel/service/UserServiceTest.java
+++ spring/src/test/java/com/abel/service/UserServiceTest.java
@@ -33,12 +33,16 @@ public class UserServiceTest {
         UserDAO userDAO = new UserDAOImpl();
         LogInterceptor li = new LogInterceptor();
         li.setTarget(userDAO);
-        //三个参数
+        //newProxyInstance 产生一个代理对象 ，三个参数
         //1.classloader 代理对象和被代理对象应该处于同一个 classloader
         //2.接口 产生的代理对象应该实现那些接口
         //3.handel 执行代理对象方法时，应用那个handel 处理。
         //(接口中有什么方法，代理中就有什么方法 代理中的每个方法在调用的时候都会把 方法自身传给 handel, 并把 代理对象和参数都传递过去 )
-        UserDAO userDAOProxy = (UserDAO) Proxy.newProxyInstance(userDAO.getClass().getClassLoader(), userDAO.getClass().getInterfaces(), li);
+        
+//      UserDAO userDAOProxy = (UserDAO) Proxy.newProxyInstance(userDAO.getClass().getClassLoader(), userDAO.getClass().getInterfaces(), li);
+        Class[] cl =new Class[1];
+        cl[0] = UserDAO.class;
+        UserDAO userDAOProxy = (UserDAO) Proxy.newProxyInstance(userDAO.getClass().getClassLoader(),cl,li);
         System.out.println(userDAOProxy.getClass());
         userDAOProxy.detele();
         userDAOProxy.save(new User());
